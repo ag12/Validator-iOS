d0fa8675efe433ff8a02c60275d4d566ee2785a9
Canopus <Canopus@Soheils-MacBook-Pro.local>
2012-12-11 22:49:46 -0500

    Updated and implemented most part of it.

--- iRegex/RWSecondViewController.m
+++ iRegex/RWSecondViewController.m
@@ -10,6 +10,7 @@
 
 @interface RWSecondViewController () <UITextFieldDelegate>
 @property (strong, nonatomic) NSArray *textFields;
+@property (retain, nonatomic) NSArray *validations;
 @property (weak, nonatomic) IBOutlet UITextField *firstNameTextField;
 @property (weak, nonatomic) IBOutlet UITextField *middleInitialTextField;
 @property (weak, nonatomic) IBOutlet UITextField *lastNameTextField;
@@ -21,6 +22,9 @@
 @end
 
 @implementation RWSecondViewController
+
+#pragma mark
+#pragma mark - View life cycle
 
 - (void)viewDidLoad
 {
@@ -37,35 +41,133 @@
                                                 self.passwordTextField,
                                                 self.emailTextField,
                                                 nil];
+    
+    // Array of regex to validate each field
+    self.validations = [NSArray arrayWithObjects:@"^[a-zA-Z]", // First name
+                                                 @"", // Middle name
+                                                 @"", // Last name
+                                                 @"", // Social security number
+                                                 @"", // Date of birth
+                                                 @"", // Username
+                                                 @"", // Password
+                                                 @"", // Email address
+                                                 nil];
 }
 
 #pragma mark
 #pragma mark - UITextField delegates
 
-
+// Called when user taps next button
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
 {
-    [textField resignFirstResponder];
+    // Index of current text field
+    NSInteger index = [self.textFields indexOfObject:textField];
+    
+    // Get the validate string and validate the content
+    NSString *validationPattern = [self.validations objectAtIndex:index];
+    [self validateTextField:textField withPattern:validationPattern];
     
     // Find the next textfield
-    NSInteger index = [self.textFields indexOfObject:textField];
     if (index < self.textFields.count - 1)
         index ++;
     else
         index = 0;
+    
+    // The next text field as the next responder
     UITextField *nextResponder = [self.textFields objectAtIndex:index];
     
     // Find the respective (hosting) cell
     // and scroll to that
     UITableViewCell *cell = (UITableViewCell *)nextResponder.superview.superview;
-    NSIndexPath *indexPath = [self.tableView indexPathForCell:cell];
-    [self.tableView scrollToRowAtIndexPath:indexPath atScrollPosition:UITableViewScrollPositionTop animated:YES];
-    
+    CGRect cellFrame = cell.frame;
+    [self.tableView scrollRectToVisible:cellFrame animated:YES];
     
     // Make it the first responder
     [nextResponder becomeFirstResponder];
     
     return YES;
+}
+
+// Called when user taps on clear button
+- (BOOL)textFieldShouldClear:(UITextField *)textField
+{
+    // We are going to return YES and clear out
+    // but the field is not cleared yet. Because for correct validation
+    // icon on the right side of the text field, we need the text field to
+    // have a cleat text, so we set it manually to nil.
+    textField.text = nil;
+    
+    // Validate
+    [self validateTextField:textField];
+    
+    return YES;
+}
+
+// Called when user taps on a different text field and
+// this text field resigns
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    // Validate
+    [self validateTextField:textField];
+}
+
+#pragma mark
+#pragma mark - Validation
+
+- (void)validateTextField:(UITextField *)textField
+{
+    // Index of current text field
+    NSInteger index = [self.textFields indexOfObject:textField];
+    
+    // Get the validate string and validate the content
+    NSString *validationPattern = [self.validations objectAtIndex:index];
+    [self validateTextField:textField withPattern:validationPattern];
+}
+
+- (void)validateTextField:(UITextField *)textField withPattern:(NSString *)pattern
+{
+    NSString *text = textField.text;
+    UIImageView *rightView = (UIImageView *)textField.rightView;
+    
+    // If user completely deletes a field, we don't want to display anything
+    if (!text.length)
+    {
+        rightView = nil;
+        textField.rightViewMode = UITextFieldViewModeNever;
+    }
+    else
+    {
+        NSError *error = nil;
+        NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:pattern options:NSRegularExpressionCaseInsensitive error:&error];
+        
+        if (!regex)
+        {
+            NSAssert(FALSE, @"Unable to create regular expression");
+        }
+        
+        NSRange textRange = NSMakeRange(0, text.length);
+        NSRange matchRange = [regex rangeOfFirstMatchInString:text options:0 range:textRange];
+        
+        if (!rightView)
+        {
+            rightView = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, 0.0, 30.0, 30.0)];
+            textField.rightView = rightView;
+            CGRect frame = [textField rightViewRectForBounds:rightView.bounds];
+            rightView.frame = frame;
+        }
+        
+        // Did it validate?
+        if (matchRange.location == NSNotFound)
+        {
+            rightView.image = [UIImage imageNamed:@"exclamation.png"];
+            textField.rightViewMode = UITextFieldViewModeUnlessEditing;
+        }
+        else
+        {
+            rightView.image = [UIImage imageNamed:@"checkmark.png"];
+            textField.rightViewMode = UITextFieldViewModeUnlessEditing;
+        }
+    }
 }
 
 @end
